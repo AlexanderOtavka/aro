ccf2588b81577c3371d0c94a5a4046fc5be8fcd8
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-01-26 18:23:59 -0600

    Add command line parsing

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,26 @@
-fn main() {
-    println!("Hello, world!");
-}
+extern crate clap;
 
-#[test]
-fn main_test() {
-    assert!(true);
+fn main() {
+    let options = clap::App::new("The aro-> Compiler")
+        .version("0.1.0")
+        .author("Zander Otavka <otavkaal@grinnell.edu")
+        .arg(
+            clap::Arg::with_name("length")
+                .help("Print out the lengths of each arg.")
+                .long("length"),
+        )
+        .arg(
+            clap::Arg::with_name("args")
+                .help("A mysterious list of arguments to be printed back.")
+                .multiple(true),
+        )
+        .get_matches();
+
+    for arg in options.values_of("args").unwrap_or_default() {
+        if options.is_present("length") {
+            println!("{}", String::from(arg).len())
+        } else {
+            println!("{}", arg);
+        }
+    }
 }
