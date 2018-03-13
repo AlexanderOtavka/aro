ce7fb2fafd3378fa7b9b08bda306bd7d4c7f002d
Zander Otavka <AlexanderOtavka@users.noreply.github.com>
2018-03-13 17:41:54 -0500

    Add floordiv and list operations as globals

--- src/globals.rs
+++ src/globals.rs
@@ -9,6 +9,41 @@ pub fn get_globals() -> HashMap<String, (Value, Type)> {
 
     sources.insert("inf", "1/0");
     sources.insert("nan", "0/0");
+    sources.insert(
+        "floordiv",
+        r#"
+            a: Num -(Num -> Int)->
+            b: Num -Int->
+                (a b) |> @hook("std.math.floordiv"  (Num Num) -> Int)
+        "#,
+    );
+    sources.insert(
+        "push",
+        r#"
+            T: Any -(T -> [T..] -> [T..])->
+            el: T -([T..] -> [T..])->
+            list: [T..] -[T..]->
+                (el list) |> @hook("std.list.push"  ((T  [T..]) -> [T..]))
+        "#,
+    );
+    sources.insert(
+        "is_empty",
+        r#"
+            @hook("std.list.is_empty" (T: Any -> [T..] -> Bool))
+        "#,
+    );
+    sources.insert(
+        "head",
+        r#"
+            @hook("std.list.head" (T: Any -> [T..] -> T))
+        "#,
+    );
+    sources.insert(
+        "tail",
+        r#"
+            @hook("std.list.tail" (T: Any -> [T..] -> [T..]))
+        "#,
+    );
 
     let mut globals = HashMap::new();
     for (name, source) in sources {
